bf8fe3fffd48b262905d9a8a9ec6ac62d51684ac
VIMER <top.michan@mail.ru>
2025-09-26 14:36:01 +0500

    interceptors in axios

--- src/app/axios/index.ts
+++ src/app/axios/index.ts
@@ -1,5 +1,4 @@
 import axios, { type AxiosRequestConfig, type AxiosResponse } from "axios";
-
 const baseURL = "http://localhost:2335/api/";
 
 const $api = axios.create({
@@ -9,15 +8,43 @@ const $api = axios.create({
     "Content-Type": "application/json",
   },
 });
-$api.interceptors.request.use((conf) => {
-  conf.headers.aut = sessionStorage.getItem("accessToken");
 
+let isRefreshing = false;
+
+$api.interceptors.request.use((conf) => {
+  conf.headers.auth = sessionStorage.getItem("accessToken");
   return conf;
 });
-$api.interceptors.response.use((conf): AxiosResponse => {
-  //   conf.data = "f";
-  console.log(conf);
-  return conf;
-});
+$api.interceptors.response.use(
+  (res: AxiosResponse) => res,
+  async (error) => {
+    if (error.response?.status === 401 && !isRefreshing && !error.request.responseURL.includes("updatAccessToken")) {
+      isRefreshing = true;
+      try {
+        const newToken = await updatToken();
+        if (newToken) {
+          error.config.headers.auth = newToken.data.accessToken;
+          return $api.request(error.config);
+        }
+      } catch (err) {
+        console.error("Не удалось обновить токен:", err);
+        window.location.reload();
+      } finally {
+        isRefreshing = false;
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+async function updatToken(): Promise<any> {
+  try {
+    const res = await $api.get("/auth/updatAccessToken");
+    sessionStorage.setItem("accessToken", res.data.accessToken);
+    return res;
+  } catch (err) {
+    throw err;
+  }
+}
 
 export default $api;
--- src/pages/main/index.vue
+++ src/pages/main/index.vue
@@ -1,1 +1,19 @@
-<template>main</template>
+<template>
+  main
+  <button @click="t">сенд</button>
+</template>
+
+<script setup lang="ts">
+import $api from "@/app/axios";
+
+function t() {
+  $api
+    .post("/domus")
+    .then((e) => {
+      console.log(e);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+</script>
